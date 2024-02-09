dd298fcefb4489f54c74fb7c47d386f13695bdbe
Sourav <dsourav@Souravs-MacBook-Air.local>
2024-02-09 23:24:02 +0530

    gcs artifact registory

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      version = ">= 4.51.0"
+    }
+  }
+}
+
+resource "google_project_service" "artifactregistry" {
+  project = "braided-pride-413213"
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_storage_bucket" "terraform-bucket" {
+  name     = "braided-pride-413213-terraform-metadata"
+  project  = "braided-pride-413213"
+  location = "us-central1"
+}
+
+
+resource "google_artifact_registry_repository" "sourav-registry" {
+  project       = "braided-pride-413213"
+  location      = "us-central1"
+  repository_id = "docker-repository"
+  description   = "example docker repository"
+  format        = "DOCKER"
+  depends_on    = [google_project_service.artifactregistry]
+}
